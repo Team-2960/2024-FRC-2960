a7181f02ea25d981fed61c1d5e35dd0a7790cf4c
Ryan Fitz-Gerald <ryan.f@pariusa.com>
2024-02-27 14:36:27 -0500

    Implemented Pizzabox Control

--- src/main/java/frc/robot/subsystems/IntakePizzaBox.java
+++ src/main/java/frc/robot/subsystems/IntakePizzaBox.java
@@ -10,37 +10,84 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class IntakePizzaBox extends SubsystemBase{
+    public enum PizzaboxState {
+        IDLE,
+        INTAKE,
+        SHOOT,
+        REVERSE
+    }
+
     private static IntakePizzaBox intake = null;
+
     private TalonFX intakeRollers;
+
     private CANSparkMax shooter1;
     private CANSparkMax shooter2;
+
+    private RelativeEncoder shootEncoder1;
+    private RelativeEncoder shootEncoder2;
+
     private DigitalInput photoeye;
-    private SimpleMotorFeedforward intakeFeedForward;
+
+    PizzaboxState state;
 
     private IntakePizzaBox(){
-       TalonFX intakeRollers = new TalonFX(0);
-       CANSparkMax shooter1 = new CANSparkMax(0, MotorType.kBrushless);
-       CANSparkMax shooter2 = new CANSparkMax(0, MotorType.kBrushless);
-       DigitalInput photoeye = new DigitalInput(0);
-    }
-        
-    public void setShooter(double speed){
-        shooter1.set(speed);
-        shooter2.set(-speed);
-        //TODO Test motors and reverse speeds if needed
+       intakeRollers = new TalonFX(0);
+
+       shooter1 = new CANSparkMax(0, MotorType.kBrushless);
+       shooter2 = new CANSparkMax(0, MotorType.kBrushless);
+       shooter2.follow(shooter1, true);
+
+       shootEncoder1 = shooter1.getEncoder();
+       shootEncoder2 = shooter1.getEncoder();
+
+       photoeye = new DigitalInput(0);
+       
+       state = IDLE;
     }
 
-    public void setIntake(double speed, boolean override){
-        double feedForward = intakeFeedForward.calculate(intakeRollers.getVelocity().getValueAsDouble());
-        if(photoeye.get() == false){
-            intakeRollers.set(speed + feedForward);
-        }else if (photoeye.get() && override == false){
-            intakeRollers.set(0);
-        }else if(photoeye.get() && override == true){
-            intakeRollers.set(speed + feedForward);
-        }
+    public void setState(PizzaboxState state) {
+        this.state = state;
     }
-    public static IntakePizzaBox get_intake() {
+
+    public PizzaboxState getState() {
+        return state;
+    }
+
+    public boolean isNotePresent() {
+        return photoeye.get();
+    }
+    
+    @override
+    public void periodic() {
+        if(state == INTAKE) {
+            if (isNotePresent()) {
+                intake.set(1);
+            } else {
+                intake.set(0);
+            }
+        } else if (state == SHOOT) {
+            shooter1.set(1);
+
+            double minShootSpeed = 500;     // TODO Move to constants
+            
+            if(shootEncoder1.getVelocity() > minShootSpeed && shootEncoder2.getVelocity() > minShootSpeed) {
+                intake.set(1);
+            }else{
+                intake.set(0);
+            }
+ 
+        } else if (state == REVERSE) {
+            shooter1.set(-1);
+            intake.set(-1);
+        } else {
+            shooter1.set(0);
+            intake.set(0);
+        }
+
+    }
+
+    public static IntakePizzaBox getInstance() {
         if (intake == null) {
             intake = new IntakePizzaBox();
         }
