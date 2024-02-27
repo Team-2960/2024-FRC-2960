d9107b104473629c014ce7f82acb9aacf1bd35fd
Ryan Fitz-Gerald <ryan.f@pariusa.com>
2024-02-27 11:09:42 -0500

    Updated demo auton to work with new commands

--- src/main/java/frc/robot/Auton/forwardAuton.java
+++ src/main/java/frc/robot/Auton/forwardAuton.java
@@ -6,21 +6,31 @@ import com.ctre.phoenix6.configs.GyroTrimConfigs;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.Auton.Commands.goToPosition;
+import frc.robot.Auton.Commands.goToAngle;
 
-public class forwardAuton extends SequentialCommandGroup {
-    public forwardAuton(String url) throws IOException{
-        addCommands(
-            new goToPosition(0, 5, new Rotation2d(0), 10, .5),
-            new goToPosition(5.4, 5, new Rotation2d(), 10, .5),
-            new goToPosition(5, 0, new Rotation2d(), 10, 0.5),
-            new goToPosition(0, 0, new Rotation2d(), 10, 0.1)
-          /*  new goToPosition(-1.09, 1.8, new Rotation2d(0), 10, 0.2),
-           new goToPosition(-1.09, 5.28, new Rotation2d(), 10, 0.2),
-           new goToPosition(5.2, 5.28, new Rotation2d(), 10, .2)*/
-
-           //new goToPosition(0, 0, new Rotation2d(), 5, .1)
+public class forwardAuton {
+    public Command getCommand() {
+        Command command = new SequentialCommandGroup(
+            new ParallelCommandGroup(
+                new goToPosition(new Translation2d(0, 5), 10, .5),
+                new goToAngle(new Rotation2d(), new Rotation2d(.5))
+            ),
+            new ParallelCommandGroup(
+                new goToPosition(new Translation2d(5.4, 5), 10, .5),
+                new goToAngle(new Rotation2d(), new Rotation2d(.5))
+            ),
+            new ParallelCommandGroup(
+                new goToPosition(new Translation2d(5, 0), 10, .5),
+                new goToAngle(new Rotation2d(), new Rotation2d(.5))
+            ),
+            new ParallelCommandGroup(
+                new goToPosition(new Translation2d(0, 0), 10, .5),
+                new goToAngle(new Rotation2d(), new Rotation2d(.5))
+            )
         );
 
+        return command;
     }
 }
