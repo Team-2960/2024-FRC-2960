7cfff9dbaf8d30f0a8a5652c18f1b74bbd5045f7
Ryan Fitz-Gerald <ryan.f@pariusa.com>
2024-02-29 10:27:29 -0500

    Added Shuffleboard to Camera

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -23,7 +23,8 @@ import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import edu.wpi.first.wpilibj.Timer;
+import frc.robot.Constants; 
 
 public class Camera extends SubsystemBase {
     private static Camera vision = null;
@@ -35,17 +36,26 @@ public class Camera extends SubsystemBase {
     private Transform3d robotToCamera;
 
     private PhotonPoseEstimator photonPoseEstimator;
-            
+    
+    private Pose2d lastPose;
     private double lastTimeStamp;
 
     private Transform2d fieldCenterOffset = new Transform2d(-8.270875, -4.105275, new Rotation2d(0.0)); // TODO Move to constants
+
+    private GenericEntry sb_PoseX;
+    private GenericEntry sb_PoseY;
+    private GenericEntry sb_PoseR;
+    private GenericEntry sb_lastTimestamp;
+    private GenericEntry sb_lastUpdatePeriod;
 
     /**
      * Constructor
      */
     private Camera() {
+        // Initialize Camera
         camera = new PhotonCamera("Camera_Module_v1");
 
+        // Get apriltag layout
         aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
         
         // TODO Initialize transform 3d in constants
@@ -53,10 +63,21 @@ public class Camera extends SubsystemBase {
                 new Translation3d(Constants.cameraToRobotX, Constants.cameraToRobotY, Constants.cameraToRobotHeight),
                 new Rotation3d(Constants.cameraRoll, Constants.cameraPitch, Constants.cameraYaw));  
 
+        // Initialize camera pose estimator
         photonPoseEstimator = new PhotonPoseEstimator(aprilTagFieldLayout,
                 PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, camera, robotToCamera);
-                
+        
+        // Initialize class variables
+        lastPose = new Pose2d();
         lastTimeStamp = 0;
+
+        // Setup Shuffleboard
+        var layout = Shuffleboard.getTab("Status").getLayout("Camera");
+        sb_PoseX = layout.add("Pose X", 0);
+        sb_PoseY = layout.add("Pose Y", 0);
+        sb_PoseR = layout.add("Pose R", 0);
+        sb_lastTimestamp = layout.add("Last Timestamp", lastTimeStamp);
+        sb_lastUpdatePeriod = layout.add("Time Since Last Update", 0);
     }
 
     /**
@@ -64,11 +85,15 @@ public class Camera extends SubsystemBase {
      */
     @Override
     public void periodic() {
-        var estPoseUpdate = photonPoseEstimator.update();
+        updatePose();
+        updateUI();
+    }
 
-        // TODO move to shuffle board
-        SmartDashboard.putBoolean("IsPresent", estPoseUpdate.isPresent());
-        SmartDashboard.putNumber("thenumberzero", 0);
+    /**
+     * Retrieves camera pose estimation updates
+     */
+    private void updatePose() {
+        var estPoseUpdate = photonPoseEstimator.update();
 
         // Check if an AprilTag is visible
         if (estPoseUpdate.isPresent()) {
@@ -78,15 +103,26 @@ public class Camera extends SubsystemBase {
             // Check if the camera has a new value
             double ts = poseUpdate.timestampSeconds;
             if (lastTimeStamp < ts) {
-                Pose2d pose_est = poseUpdate.estimatedPose.toPose2d().transformBy(fieldCenterOffset);
+                lastPose = poseUpdate.estimatedPose.toPose2d().transformBy(fieldCenterOffset);
 
                 // Update drivetrain pose estimation
-                Drive.getInstance().setVisionPose(pose_est, ts);
+                Drive.getInstance().setVisionPose(lastPose, ts);
 
                 // Update last timestamp
                 lastTimeStamp = ts;
             }
         }
+    }
+
+    /**
+     * Updates Shuffleboard
+     */
+    private void updateUI() {
+        sb_PoseX.setDouble(lastPose.getX());
+        sb_PoseY.setDouble(lastPose.getY());
+        sb_PoseR.setDouble(lastPose.getRotation().getDegrees());
+        sb_lastTimestamp.setDouble(lastTimeStamp);
+        sb_lastUpdatePeriod.setDouble(Timer.getFPGATimestamp() - lastTimeStamp);
     }
     
     /**
