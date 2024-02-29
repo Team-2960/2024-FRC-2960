af8cd8c5331f1d2cc0f925f465cc0eec145094b3
Ryan Fitz-Gerald <ryan.f@pariusa.com>
2024-02-29 14:18:29 -0500

    Added PID and Feed Forward parameter classes

---
+++ src/main/java/frc/robot/Util/PIDParam.java
@@ -0,0 +1,13 @@
+package frc.robot.util;
+
+public class PIDParam {
+    public double kP;
+    public double kI;
+    public double kD;
+
+    public PIDParam(double kP, double kI, double kD) {
+        this.kP = kP;
+        this.kI = kI;
+        this.kD = kD;
+    }
+}
