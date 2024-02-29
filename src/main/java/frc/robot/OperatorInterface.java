a68bce329af0bf8d91a93329f1221eaa0949bf18
Ryan Fitz-Gerald <ryan.f@pariusa.com>
2024-02-29 10:40:19 -0500

    Cleaned up and added shuffleboard to OperatorInterface

--- src/main/java/frc/robot/OperatorInterface.java
+++ src/main/java/frc/robot/OperatorInterface.java
@@ -14,54 +14,92 @@ public class OperatorInterface extends SubsystemBase {
     public static OperatorInterface oi = null;
 
     // JOYSTICKS
-    private static Joystick driverController;
-    private static Joystick operatorController;
+    private Joystick driverController;
+    private Joystick operatorController;
 
+    // Shuffleboard Entries
+    private GenericEntry sb_driveX;
+    private GenericEntry sb_driveY;
+    private GenericEntry sb_driveR;
+    private GenericEntry sb_driveFR;
+
+    /**
+     * Constructor
+     */
     private OperatorInterface() {
-        // Initiating Subystems
-
         // Create Joysticks
         driverController = new Joystick(0);
         operatorController = new Joystick(1);
 
+        // Setup Shuffleboard
+        var drive_layout = Shuffleboard.getTab("OI").getLayout("Drive");
+        sb_driveX = drive_layout.add("X Speed", 0);
+        sb_driveY = drive_layout.add("Y Speed", 0);
+        sb_driveR = drive_layout.add("R Speed", 0);
+        sb_driveFR = drive_layout.add("Field Relative", true);
     }
 
+    /**
+     * Subsystem Period Method
+     */
     @Override
     public void periodic() {
-
         if (DriverStation.isTeleop()) {
-            boolean fieldRelative = !driverController.getRawButton(1);
-            
-
-            double xSpeed = MathUtil.applyDeadband(driverController.getRawAxis(0), 0.1) * Constants.kMaxSpeed;
-            double ySpeed = -MathUtil.applyDeadband(driverController.getRawAxis(1), 0.1) * Constants.kMaxSpeed;
-            double rSpeed =  MathUtil.applyDeadband(driverController.getRawAxis(4), 0.1) * Constants.kMaxAngularSpeed;
-
-            
-            Drive drive = Drive.getInstance();
-            
-            drive.setfieldRelative(fieldRelative);
-            drive.setSpeed(xSpeed, ySpeed);    
-            drive.setAngleRate(rSpeed);
-                        
-            SmartDashboard.putNumber("ySpeed", ySpeed);
-            SmartDashboard.putNumber("xSpeed", xSpeed);
-            SmartDashboard.putNumber("rSpeed", rSpeed);
-            SmartDashboard.putBoolean("FieldRelative", fieldRelative);
-
-          /*   if (driverController.getRawAxis(5)<0.2 && driverController.getRawAxis(5)>-0.2){
-                rSpeed = 0;
-            }
-            */
+            updateDrive();
+            updateArm();
+            updatePizzabox();
+            updateClimber();
         }
+    }
+    
+    /**
+     * Updates the controls for the drivetrain
+     */
+    private void updateDrive() {
+        boolean fieldRelative = !driverController.getRawButton(1);
+        
+        double xSpeed = MathUtil.applyDeadband(driverController.getRawAxis(0), 0.1) * Constants.kMaxSpeed;
+        double ySpeed = -MathUtil.applyDeadband(driverController.getRawAxis(1), 0.1) * Constants.kMaxSpeed;
+        double rSpeed =  MathUtil.applyDeadband(driverController.getRawAxis(4), 0.1) * Constants.kMaxAngularSpeed;
 
-        // Arm Control
-      
+        Drive drive = Drive.getInstance();
+        
+        drive.setfieldRelative(fieldRelative);
+        drive.setSpeed(xSpeed, ySpeed);    
+        drive.setAngleRate(rSpeed);
 
-
-
+        // Update Shuffleboard
+        sb_driveX.setDouble(xSpeed);
+        sb_driveY.setDouble(ySpeed);
+        sb_driveR.setDouble(rSpeed);
+        sb_driveFR.setBoolean(fieldRelative);
     }
 
+    /**
+     * Updates the controls for the arm
+     */
+    private void updateArm() {
+        // TODO Implement arm controls
+    }
+
+    /**
+     * Updates the controls for the Pizzabox
+     */
+    private void updatePizzabox() {
+        // TODO implement pizzabox controls
+    }
+
+    /**
+     * Updates the controls for the climber
+     */
+    private void updateClimber() {
+        // TODO implement climber controls
+    }
+
+    /**
+     * Static Initializer
+     * @return  common instance of the OperatorInterface class
+     */
     public static OperatorInterface getInstance() {
         if (oi == null) {
             oi = new OperatorInterface();
