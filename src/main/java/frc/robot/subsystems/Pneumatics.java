d880eda5108b0fe1fb22ec2b7241ad37cf046e11
Ryan Fitz-Gerald <ryan.f@pariusa.com>
2024-02-29 10:15:37 -0500

    Added shuffleboard to pneumatics

--- src/main/java/frc/robot/subsystems/Pneumatics.java
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -11,8 +11,26 @@ public class Pneumatics extends SubsystemBase {
 
     public Compressor compressor;
 
+    private GenericEntry sb_pressure;
+    private GenericEntry sb_current;
+
     private Pneumatics() {
         compressor = new Compressor(20,PneumaticsModuleType.REVPH);
+        compressor.enableAnalog(80, 120);
+
+        var layout = Shuffleboard.getTab("Status").getLayout("Pneumatics");
+        sb_pressure = layout.add("Pressure");
+        sb_current = layout.add("Current");
+    }
+
+    @Override
+    public void periodic() {
+        updateUI();
+    }
+
+    public void updateUI() {
+        sb_pressure.setDouble(compressor.getPressure());
+        sb_current.setDouble(compressor.getCurrent());
     }
 
     public static Pneumatics getInstance() {
