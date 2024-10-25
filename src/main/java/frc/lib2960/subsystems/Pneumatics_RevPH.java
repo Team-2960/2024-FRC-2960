466c65cc42ceb2f3a9075a56e5cbd5eb414d7719
Ryan Fitz-Gerald <ryan.f@pariusa.com>
2024-10-25 13:35:43 -0400

    Created Pneumatics_RevPH in lib2960

---
+++ src/main/java/frc/lib2960/subsystems/Pneumatics_RevPH.java
@@ -0,0 +1,136 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.PneumaticsHub;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.networktables.GenericEntry;
+
+import frc.robot.Constants;
+
+/**
+ * Manages a REV Pneumatics Hub
+ */
+public class Pneumatics_RevPH extends SubsystemBase {
+
+    /**
+     * REV Pneumatics Hub settings
+     */
+    public class Settings {
+        public static final String DEF_NAME = "Pneumatics Hub";
+        public static final int DEF_CAN_ID = 1;
+
+        public enum ControlMode {DIGITAL, ANALOG, HYBRID};
+
+        public final String name; 
+        public final int can_id;
+        public final ControlMode control_mode;
+        public final double min_pressure;
+        public final double max_pressure;
+
+        /**
+         * Constructor
+         * @param   name            name of the module
+         * @param   can_id          CAN ID for the module
+         * @param   control_mode    Module Control Mode
+         * @param   min_pressure    Minimum allowed pressure in PSI
+         * @param   max_pressure    Maximum allowed pressure in PSI
+         */
+        public Settings(String name, int can_id, ControlMode control_mode, double min_pressure, double max_pressure) {
+            this.name = name;
+            this.can_id = can_id;
+            this.control_mode = control_mode;
+            this.min_pressure = min_pressure;
+            this.max_pressure = max_pressure;
+        }
+
+        /**
+         * Constructor
+         *      - name set to DEF_NAME
+         *      - can_id set to DEF_CAN_ID
+         *      - control_mode set to DIGITAL
+         *      - min_pressure set to 0
+         *      - max_pressure set to 120 
+         */
+        public Settings() {
+            Settings(def_name, def_can_id, ControlMode.DIGITAL, 0, 120);
+        }
+    }
+
+
+    public final Settings settings;     /**< Module Settings */
+    
+    private final PneumaticsHub ph;      /**< Module objet reference*/
+
+    // ShuffleBoard
+    private GenericEntry sb_pressure;   
+    private GenericEntry sb_current;
+
+    /**
+     * Constructor. Settings set to default values;
+     * @param   enabled     determines if the compressor is enabled when object is created
+     */
+    private Pneumatics_RevPH(boolean enabled) {
+        Pneumatics_RevPH(new Settings());
+    }
+
+    /**
+     * Constructor
+     * @param   settings    Pneumatics Hub settings
+     * @param   enabled     determines if the compressor is enabled when object is created
+     */
+    private Pneumatics_RevPH(Settings settings, boolean enabled) {
+        this.settings = settings;
+
+        // Create PneumaticsHub
+        ph = PneumaticsHub(settings.can_id);
+
+        // Setup ShuffleBoard
+        var layout = Shuffleboard.getTab("Status")
+                .getLayout(settings.name, BuiltInLayouts.kList)
+                .withSize(1, 2);
+        sb_pressure = layout.add("Pressure", 0).getEntry();
+        sb_current = layout.add("Current", 0).getEntry();
+
+        // Set Compressor Enabled
+        steEnabled(enabled);
+    }
+
+    /**
+     * Enables/Disables the compressor
+     * @param   enabled     true to enable the compressor, false to disable.
+     */
+    public void enableCompressor(boolean enabled) {
+        if(enabled) {
+            switch(settings.control_mode) {
+                case ControlMode.DIGITAL:
+                    ph.enableCompressorDigital();
+                    break;
+                case ControlMode.ANALOG:
+                    ph.enableCompressorAnalog(settings.min_pressure, settings.max_pressure);
+                    break;
+                case ControlMode.HYBRID:
+                    ph.enableCompressorHybrid(settings.min_pressure, settings.max_pressure);
+                    break;
+            }
+        }else{
+            ph.disableCompressor();
+        }
+    }
+
+    /**
+     * Periodic method. Updates UI.
+     */
+    @Override
+    public void periodic() {
+        updateUI();
+    }
+
+    /**
+     * Updates ShuffleBoard
+     */
+    public void updateUI() {
+        sb_pressure.setDouble(compressor.getPressure(0));
+        sb_current.setDouble(ph.getCompressorCurrent());
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/Pneumatics.java
@@ -0,0 +1,40 @@
+package frc.robot.subsystems;
+
+import frc.robot.subsystems.Pneumatics_RevPH;
+
+import frc.robot.Constants;
+
+public class Pneumatics {
+    private static Pneumatics instance = null;      /**< Static singleton instance */
+
+    public Pneumatics_RevPH ph;                     /**< Pneumatics Hub instance */
+
+    /**
+     * Constructor
+     */
+    private Pneumatics() {
+        ph = Pneumatics_RevPH(
+            new Pneumatics_RevPH.Settings(
+                Pneumatics_RevPH.def_name,
+                Constants.phCANID,
+                Pneumatics_RevPH.Settings.ANALOG,
+                Constants.minPressure,
+                Constants.maxPressure
+            )
+        )
+
+            
+        compressor = new Compressor(Constants.phCANID, PneumaticsModuleType.REVPH);
+        compressor.enableAnalog(Constants.minPressure, Constants.maxPressure);
+    }
+
+    /**
+     * Retrieves the robot's instance of the Pneumatics class
+     * @return  robot's instance of the Pneumatics class
+     */
+    public static Pneumatics getInstance() {
+        if (instance == null) instance = new Pneumatics();
+
+        return instance;
+    }
+}
