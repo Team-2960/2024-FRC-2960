0645bcd43eb7fc79117e54a9ad973763b8874b88
Ryan Fitz-Gerald <ryan.f@pariusa.com>
2024-10-29 09:07:42 -0400

    Added stubs for other robot mechanism generalizations

---
+++ src/main/java/frc/lib2960/subsystems/IntakeStageBase.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2024 Ryan Fitz-Gerald
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+ * sell copies of the Software, and to permit persons to whom the Software is 
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+ package ftc.lib2960.subsystems;
+
+ import frc.lib2960.util.*;
+ import frc.lib2960.controllers.*;
+ 
+ import edu.wpi.first.math.geometry.*;
+ import edu.wpi.first.wpilibj2.command.*;
+ import edu.wpi.first.math.kinematics.*;
+ import edu.wpi.first.networktables.*;
+
+ /**
+  * Manages a one stage of an intake system
+  */
+public class IntakeStageBase extends SubsystemBase{
+    // TODO Implement
+}
